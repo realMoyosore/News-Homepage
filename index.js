1562948c1e648f2121a941198f2aaa001a7254ab
Moyosore Sulola <moyixxoreoluwa@gmail.com>
2025-06-24 21:10:26 +0100

    updated

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-// const toggleButton = document.querySelector('.menu-toggle');
-// const navLinks = document.querySelector('nav-links');
-
-// toggleButton.addEventListener('click', () => {
-//     toggleButton.classList.toggle('active');
-//     navLinks.classList.toggle('show');
-// }
-// );
-
 const menuToggle = document.getElementById("menuToggle");
 const mobileNav = document.getElementById("mobileNav");
 const closeMenu = document.getElementById("closeMenu");

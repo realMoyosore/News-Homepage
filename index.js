f006d00f1a3cac656e91ed059834bd3741b2b7b1
Moyosore Sulola <moyixxoreoluwa@gmail.com>
2025-06-24 20:04:31 +0100

    updated

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-const toggleButton = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('nav-links');
+// const toggleButton = document.querySelector('.menu-toggle');
+// const navLinks = document.querySelector('nav-links');
 
-toggleButton.addEventListener('click', () => {
-    toggleButton.classList.toggle('active');
-    navLinks.classList.toggle('show');
-}
-);
+// toggleButton.addEventListener('click', () => {
+//     toggleButton.classList.toggle('active');
+//     navLinks.classList.toggle('show');
+// }
+// );
+
+
+const menuToggle = document.getElementById('menuToggle');
+const mobileNav = document.getElementById('mobileNav');
+const closeMenu = document.getElementById('closeMenu');
+const menuOverlay = document.getElementById('menuOverlay');
+
+menuToggle.addEventListener('click', () => {
+  mobileNav.classList.add('show');
+  menuOverlay.classList.add('active');
+});
+closeMenu.addEventListener('click', () => {
+  mobileNav.classList.remove('show');
+  menuOverlay.classList.remove('active');
+});
+menuOverlay.addEventListener('click', () => {
+  mobileNav.classList.remove('show');
+  menuOverlay.classList.remove('active');
+});

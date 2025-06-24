2f8aac48fbaab4b629149c597068676390543db5
Moyosore Sulola <moyixxoreoluwa@gmail.com>
2025-06-24 20:38:44 +0100

    menuToggle edited

--- index.js
+++ index.js
@@ -7,21 +7,23 @@
 // }
 // );
 
+const menuToggle = document.getElementById("menuToggle");
+const mobileNav = document.getElementById("mobileNav");
+const closeMenu = document.getElementById("closeMenu");
+const menuOverlay = document.getElementById("menuOverlay");
 
-const menuToggle = document.getElementById('menuToggle');
-const mobileNav = document.getElementById('mobileNav');
-const closeMenu = document.getElementById('closeMenu');
-const menuOverlay = document.getElementById('menuOverlay');
-
-menuToggle.addEventListener('click', () => {
-  mobileNav.classList.add('show');
-  menuOverlay.classList.add('active');
+menuToggle.addEventListener("click", () => {
+  mobileNav.classList.add("show");
+  menuOverlay.classList.add("active");
+  menuToggle.classList.add("hide");
 });
-closeMenu.addEventListener('click', () => {
-  mobileNav.classList.remove('show');
-  menuOverlay.classList.remove('active');
+closeMenu.addEventListener("click", () => {
+  mobileNav.classList.remove("show");
+  menuOverlay.classList.remove("active");
+  menuToggle.classList.remove("hide");
 });
-menuOverlay.addEventListener('click', () => {
-  mobileNav.classList.remove('show');
-  menuOverlay.classList.remove('active');
+menuOverlay.addEventListener("click", () => {
+  mobileNav.classList.remove("show");
+  menuOverlay.classList.remove("active");
+  menuToggle.classList.remove("hide");
 });
